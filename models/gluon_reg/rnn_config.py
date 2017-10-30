45a7186bbcd5918dcd83068e7ea9e7687d143d01
danxifuer <daiab_piao@163.com>
2017-10-30 22:55:30 +0800

    commit

--- models/gluon_reg/rnn_config.py
+++ models/gluon_reg/rnn_config.py
@@ -21,7 +21,7 @@ INPUT_SIZE = 7
 CELL_TYPE = 'rnn_tanh'
 TRAIN_DATA_PATH = '/home/daiab/machine_disk/code/quantum/photon/ohlcvr_from_normed_data_50_length.rec'
 # INFER_DATA_PATH = ''
-RESTORE_PATH = '/home/daiab/machine_disk/code/quantum/models/gluon_cls_use_across_data/save/mx_model'
+RESTORE_PATH = '/home/daiab/machine_disk/code/quantum/models/gluon_reg/save/mx_model'
 
 
 # infer
