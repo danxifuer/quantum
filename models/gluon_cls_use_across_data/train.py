bd294612286451f3b5d7f145e3478da349466831
daianbo <anbo.dai@seetatech.com>
2017-11-07 16:36:13 +0800

    commit

--- models/gluon_cls_use_across_data/train.py
+++ models/gluon_cls_use_across_data/train.py
@@ -1,10 +1,11 @@
-import time
+import numpy as np
 import math
 import mxnet as mx
 from mxnet import gluon, autograd
 import model
 import logging
 from rnn_config import *
+import matplotlib.pyplot as plt
 from dataiter import DataIter, InferDataIter
 
 logging.basicConfig(level=logging.DEBUG,
@@ -41,7 +42,7 @@ def detach(hidden):
 
 
 LOG_INTERVAL = 40
-CLIP = 0.2
+CLIP = 0.1
 
 
 # model.collect_params().load(RESTORE_PATH, context)
@@ -53,7 +54,8 @@ class ModelTrain:
         self.model = model.RNNModel(mode=CELL_TYPE, num_embed=INPUT_SIZE,
                                     num_hidden=HIDDEN_UNITS, seq_len=SEQ_LEN,
                                     num_layers=NUM_LAYERS, dropout=DROPOUT)
-        self.trainer = gluon.Trainer(model.collect_params(), 'sgd',
+        self._model_init()
+        self.trainer = gluon.Trainer(self.model.collect_params(), 'sgd',
                                      {'learning_rate': LR,
                                       'momentum': 0.9,
                                       'wd': 0.00001})
@@ -61,15 +63,19 @@ class ModelTrain:
         self.lr_decay = LRDecay(LR, END_LR, 0.6, DECAY_STEP)
         self.data_iter = DataIter(TRAIN_DATA_PATH, BATCH_SIZE)
         self.infer_iter = InferDataIter(INFER_DATA_PATH, BATCH_SIZE)
+        self.train_record = []
+        self.val_record = []
 
     def _model_init(self):
         self.model.collect_params().initialize(mx.init.Xavier(), ctx=self.context)
 
     def run(self):
-        hidden = model.begin_state(func=mx.nd.zeros, batch_size=BATCH_SIZE, ctx=self.context)
+        hidden = self.model.begin_state(func=mx.nd.zeros, batch_size=BATCH_SIZE, ctx=self.context)
         for epoch in range(EPOCH):
-            total_loss = 0.0
-            total_acc = 0.0
+            tmp_loss = 0.0
+            tmp_acc = 0.0
+            epoch_loss = []
+            epoch_acc = []
             total = BATCH_SIZE * PREDICT_LEN * LOG_INTERVAL
             for i in range(ITER_NUM_EPCOH):
                 data, target = self.data_iter.next()
@@ -78,31 +84,37 @@ class ModelTrain:
                 target = mx.nd.reshape(target, shape=(-1,))
                 hidden = detach(hidden)
                 with autograd.record():
-                    output, hidden = model(data, hidden)
+                    output, hidden = self.model(data, hidden)
                     L = self.loss(output, target)
                     L.backward()
 
-                grads = [i.grad(self.context) for i in model.collect_params().values()]
+                grads = [i.grad(self.context) for i in self.model.collect_params().values()]
                 # Here gradient is for the whole batch.
                 # So we multiply max_norm by batch_size and bptt size to balance it.
-                gluon.utils.clip_global_norm(grads, CLIP * BATCH_SIZE)
+                global_norm = gluon.utils.clip_global_norm(grads, CLIP * BATCH_SIZE)
 
                 self.trainer.step(BATCH_SIZE)
-                total_loss += mx.nd.sum(L).asscalar()
-                total_acc += mx.nd.sum(mx.nd.equal(mx.nd.argmax(output, axis=1), target)).asscalar()
+                tmp_loss += mx.nd.sum(L).asscalar()
+                tmp_acc += mx.nd.sum(mx.nd.equal(mx.nd.argmax(output, axis=1), target)).asscalar()
 
                 if i % LOG_INTERVAL == 0:
-                    cur_loss = total_loss / total
-                    cur_acc = total_acc / total
+                    cur_loss = tmp_loss / total
+                    cur_acc = tmp_acc / total
                     logger.info('%d # %d loss %.5f, ppl %.5f, lr %.5f, acc %.5f',
                                 epoch, i, cur_loss, math.exp(cur_loss), self.trainer._optimizer.lr, cur_acc)
-                    total_loss = 0.0
-                    total_acc = 0.0
+                    epoch_loss.append(cur_loss)
+                    epoch_acc.append(cur_acc)
+                    tmp_loss = 0.0
+                    tmp_acc = 0.0
                 self.trainer._optimizer.lr = self.lr_decay.lr
-            self.infer()
             self.model.collect_params().save(RESTORE_PATH)
+            self.valid(hidden)
+            self.train_record.append((sum(epoch_loss) / len(epoch_loss),
+                                      sum(epoch_acc) / len(epoch_acc)))
+            self.plot()
 
-    def infer(self):
+    def valid(self, hidden):
+        print('start to valid')
         total_loss = 0.0
         total_acc = 0.0
         count = 0
@@ -112,16 +124,35 @@ class ModelTrain:
                 data = mx.nd.array(data, self.context)
                 target = mx.nd.array(target, self.context)
                 target = mx.nd.reshape(target, shape=(-1,))
-                output, hidden = model(data, hidden)
+                output, _ = self.model(data, hidden)
                 L = self.loss(output, target)
                 total_loss += mx.nd.sum(L).asscalar()
                 total_acc += mx.nd.sum(mx.nd.equal(mx.nd.argmax(output, axis=1), target)).asscalar()
                 count += BATCH_SIZE
         except:
-            cur_loss = total_loss / count
-            cur_acc = total_acc / count
+            cur_loss = total_loss / count / PREDICT_LEN
+            cur_acc = total_acc / count / PREDICT_LEN
             logger.info('valid: loss %.5f, ppl %.5f, acc %.5f',
                         cur_loss, math.exp(cur_loss), cur_acc)
+            self.val_record.append((cur_loss, cur_acc))
+        print('valid over')
+
+    def plot(self):
+        train = np.array(self.train_record)
+        val = np.array(self.val_record)
+        x = np.arange(0, train.shape[0])
+
+        fig, axes = plt.subplots(ncols=2, figsize=(18, 6))
+        ax = axes[0]
+        ax.plot(x, train[:, 0], 'o-')
+        ax.plot(x, val[:, 0], '*-')
+        ax.set_title("loss")
+
+        ax = axes[1]
+        ax.plot(x, train[:, 1], 'o-')
+        ax.plot(x, val[:, 1], '*-')
+        ax.set_title("acc")
+        plt.show()
 
 
 if __name__ == '__main__':
